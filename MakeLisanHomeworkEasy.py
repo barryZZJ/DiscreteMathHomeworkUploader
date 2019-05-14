b7ed6bc86989e9c735f4c19066af6096c4bd09ce
zhouzejun2 <48206733+zhouzejun2@users.noreply.github.com>
2019-05-15 01:01:55 +0800

    Create MakeLisanHomeworkEasy.py

---
+++ MakeLisanHomeworkEasy.py
@@ -0,0 +1,9 @@
+from easyHomework import easyHomework
+domain = '.educoder.net'
+url_head = 'https://www.educoder.net'
+class_num = '001'
+username = ''
+password = ''
+
+easyHmwork = easyHomework(domain,url_head,class_num, username, password)
+easyHmwork.easeHomework()
